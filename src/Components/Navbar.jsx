14245cb37413298f2eacead8536472e0d137840d
Aurian <aurian.marvilliers@gmail.com>
2025-12-30 01:00:57 +0100

    Deploy latest build

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar({ isDark = true }) {
           <Link to="/about" className={`transition ${hoverColor}`}>À propos</Link>
         </li>
         <li>
-          <Link to="/projects" className={`transition ${hoverColor}`}>Mes projets</Link>
+          <Link to="/projects" className={`transition ${hoverColor}`}>Projets</Link>
         </li>
         <li>
           <Link to="/contact" className={`transition ${hoverColor}`}>Contact</Link>
--- src/Pages_web/Contact.jsx
+++ src/Pages_web/Contact.jsx
@@ -5,8 +5,54 @@ function Contact() {
   return (
     <div>
       <Navbar />
-      <h1>Contact Me</h1>
-      <p>This is the Contact page of our website.</p>
+
+        <div className="flex flex-col items-center justify-center w-full  bg-black pt-20">
+          <h2 className="text-white text-5xl font-bold text-center mt-8 mb-18">
+            On reste en contact ?
+          </h2>
+        </div>
+
+
+      <div className="flex items-center justify-center my-16 gap-4 px-4 md:px-16">
+        <p className="text-3xl font-medium text-center">Contactez moi !</p>
+        <div className="flex-1 h-px bg-black" />
+      </div>
+
+      <div>
+        <form className="flex flex-col items-center justify-center gap-6 mb-20">
+          <input
+            type="text"
+            placeholder="Votre prénom"
+            className="w-1/2 p-4 border border-gray-300 rounded"
+          />
+          <input
+            type="text"
+            placeholder="Votre nom"
+            className="w-1/2 p-4 border border-gray-300 rounded"
+          />
+          <input
+            type="email"
+            placeholder="Votre email"
+            className="w-1/2 p-4 border border-gray-300 rounded"
+          />
+          <input
+            type="text"
+            placeholder="Entreprise/Organisation (optionnel)"
+            className="w-1/2 p-4 border border-gray-300 rounded"
+          />
+          <textarea
+            placeholder="Votre message"
+            className="w-1/2 p-4 border border-gray-300 rounded h-40"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-black text-white px-6 py-3 rounded hover:bg-gray-800 transition"
+          >
+            Envoyer
+          </button>
+        </form>
+      </div>
+
       <Footer />
     </div>
   )
